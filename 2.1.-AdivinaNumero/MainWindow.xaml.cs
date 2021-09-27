2831b90900ff599a4a0e313be11a1a89365907a6
Ruben9911 <ruben@iesdoctorbalmis.com>
2021-09-27 13:46:20 +0200

    Creación carpeta assets

--- 2.1.-AdivinaNumero/MainWindow.xaml.cs
+++ 2.1.-AdivinaNumero/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _2._1._AdivinaNumero
 {
@@ -26,7 +14,7 @@ namespace _2._1._AdivinaNumero
         public MainWindow()
         {
             InitializeComponent();
-            
+
             numeroAAdivinar = seed.Next(1, 101);
 
         }
@@ -34,7 +22,7 @@ namespace _2._1._AdivinaNumero
         private void Button_Click_Reiniciar(object sender, RoutedEventArgs e)
         {
             numeroAAdivinar = seed.Next(1, 101);
-            mensajeTextBlock.Text = "Numero Reiniciado";
+            mensajeTextBlock.Text = String.Empty;
         }
 
         private void Button_Click_Comprobar(object sender, RoutedEventArgs e)
@@ -45,7 +33,7 @@ namespace _2._1._AdivinaNumero
                 mensajeTextBlock.Text = "Te has quedado corto";
             else
                 mensajeTextBlock.Text = "Enhorabuena has acertado!";
-           
+
         }
     }
 }
